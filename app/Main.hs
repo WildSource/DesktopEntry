ba88cede74abc59b3a23466de319ac42371ff03f
WildSource <ilovetrap123@hotmail.com>
2025-03-05 17:03:46 -0500

    in progress writeDE to return path

--- app/Main.hs
+++ app/Main.hs
@@ -3,42 +3,37 @@ module Main where
 type Scope = String
 type AppName = String
 type Content = String
+type ExecPath = String
+type IconPath = String 
 
-writeDE :: IO () 
+writeDE :: Scope -> AppName -> ExecPath -> IconPath -> IO () 
 writeDE s a e i =
-  let fileContent =    
-    "[Desktop Entry]" <>
-    "Type=Application" <>
-    "Name=" <> a <>
-    "Exec=" <> e <>
-    "Icon=" i   
-  in write s (filter (" " ==) a) fileContent
+  let fileContent = "[Desktop Entry]\n" <> "Type=Application\n" <> "Name=" <> a <> "\nExec=" <> e <> "\nIcon=" <> i   
+  in write s (filter (' ' ==) a) fileContent
   where
     write :: Scope -> AppName -> Content -> IO ()
     write "G" a c = 
       writeFile ("/usr/share/applications/" <> a <> ".desktop") c
     write "U" a c = 
       writeFile ("~/.local/share/applications/" <> a <> ".desktop") c 
+    write _ a c = 
+      error "Not a valid Scope"
+    where
+      write' = undefined
 
-validateOptions :: [String] -> IO String
-validateOptions options = 
-  let option = getLine
-  if elem option options
-  then option
-  else putStrLn "Not a valid option, try again." >> validateOptions options
-
-fileCheck :: Path -> IO ()
-fileCheck = undefined 
+fileCheck :: String -> IO Bool
+fileCheck path =
+  doesFileExist path >>= isSuccess 
+  where
+    isSuccess :: Bool -> IO ()
+    isSuccess True = putStrLn "Desktop Entry was created !"
+    isSuccess False = putStrLn "Desktop Entry's creation failed !"
 
 main :: IO ()
 main = do
   putStrLn "Global or User-Specific Desktop Entry ?" 
   putStrLn "(G/U)"
-  scope <- validateOptions [
-    "G", "U", "g", "u", 
-    "Global", "User", 
-    "global", "user"
-  ]
+  scope <- getLine
   
   putStrLn "What is the name of the app ?"
   appName <- getLine
