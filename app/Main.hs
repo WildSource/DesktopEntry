ec9a95f5dc591e7a11320a5654f70ce17fb28c3a
WildSource <ilovetrap123@hotmail.com>
2025-03-04 20:47:19 -0500

    feat: add basic app flow

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Global or User-Specific Desktop Entry ?" 
+  putStrLn "(G/U)"
+  scope <- getLine
+  
+  putStrLn "What is the name of the app ?"
+  appName <- getLine
+  
+  putStrLn "Enter the executable's Path:"
+  execPath <- getLine
+
+  putStrLn "Enter the Icon's path (optional)"
+  putStrLn "(default: none)"
+  iconPath <- getLine
+
+  putStrLn "Desktop Entry created !"
